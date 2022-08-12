9d4c4fd41314952aedb0607bc9eb3a1ca4c1ac3a
Javier <javier.de.jesus.perez.santiago@gmail.com>
2022-08-12 03:35:50 -0500

    03:35 am 12/08/2022 subi composite

---
+++ src/com/examen2/test/Examen2Test.java
@@ -0,0 +1,25 @@
+package com.examen2.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import com.examen2.composite.*;
+
+class Examen2Test {
+
+	@Test
+	void testString() {
+		String nombre = "nombre";
+		assertEquals("nombre",nombre);	
+	}
+	
+	@Test
+	void testAñadir() {
+		
+
+	}
+
+}
